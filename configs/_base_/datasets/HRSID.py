7ace75de1c9e239120f32ea5ec5eb1b57812d84b
Ziheng Jin <starhtime0715@gmail.com>
2025-03-03 16:23:25 +0800

    data url modified

--- configs/_base_/datasets/HRSID.py
+++ configs/_base_/datasets/HRSID.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'SAR_Objectness_Dataset' 
-data_root = 'data/HRSID/'
+data_root = '/root/autodl-tmp/data/HRSID/'
 
 # Example to use different file client
 # Method 1: simply set the data root and let the file I/O module
